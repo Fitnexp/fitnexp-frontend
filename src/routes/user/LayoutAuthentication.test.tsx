f50868310880f0acb586fc9bbc41c784771bd25f
JMGarCas <jmgarciacasillas135@gmail.com>
2024-08-03 20:30:07 +0200

    test: LayoutAuthentication tests

---
+++ src/routes/user/LayoutAuthentication.test.tsx
@@ -0,0 +1,26 @@
+import { expect, describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import LayoutAuthentication from '@/routes/user/LayoutAuthentication';
+
+const renderLayoutAuthentication = () => {
+    render(
+        <BrowserRouter>
+            <LayoutAuthentication />
+        </BrowserRouter>,
+    );
+};
+
+describe('Register', () => {
+    describe('when the LayoutAuthetication page is rendered', () => {
+        it('make sure that the image is present', () => {
+            renderLayoutAuthentication();
+
+            const image = screen.getByRole('img', {
+                name: 'Authentication page background gym',
+            });
+
+            expect(image).toBeInTheDocument();
+        });
+    });
+});
