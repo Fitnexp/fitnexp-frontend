bc4bc5a09e10177e1ac674fb79a673cbf5c996aa
JMGarCas <jmgarciacasillas135@gmail.com>
2024-08-03 20:32:31 +0200

    feat: Implement a functional Register page

---
+++ src/routes/user/Register.tsx
@@ -0,0 +1,29 @@
+import RegisterForm from '@/components/user/RegisterForm';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function Register() {
+    useEffect(() => {
+        document.title = 'Fitnexp - Sign Up';
+    }, []);
+
+    return (
+        <>
+            <div className="flex flex-col items-center">
+                <h1 className="text-3xl font-bold">Sign Up</h1>
+                <h2 className="text-balance text-muted-foreground">
+                    Create an account in a few steps
+                </h2>
+            </div>
+            <RegisterForm />
+            <div className="mt-4 text-center text-sm">
+                Have an account?{' '}
+                <Link to=".." className="underline">
+                    Login
+                </Link>
+            </div>
+        </>
+    );
+}
+
+export default Register;
