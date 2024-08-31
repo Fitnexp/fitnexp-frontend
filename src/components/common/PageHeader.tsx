c422419b7d157b4e7a7e798c40d356f828d92519
JMGarCas <jmgarciacasillas135@gmail.com>
2024-08-31 12:09:42 +0200

    feat: add workout exercise

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -5,11 +5,13 @@ function PageHeader({
     icon,
     onChange,
     placeholder,
+    children = null,
 }: {
     readonly title: string;
     readonly icon: React.ReactNode;
     readonly onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     readonly placeholder: string;
+    readonly children?: React.ReactNode;
 }) {
     return (
         <div className="relative bottom-0 z-10 flex flex-col xl:sticky xl:top-0">
@@ -17,6 +19,7 @@ function PageHeader({
                 {icon} {title}
             </h1>
             <SearchBar onChange={onChange} placeholder={placeholder} />
+            {children}
         </div>
     );
 }
